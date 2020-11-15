afa7e03270906bb63f47a0703425eb849a5287a2
Liam DeVoe <orionldevoe@gmail.com>
2020-11-15 16:30:55 -0500

    fix indentation

--- documentation/example.py
+++ documentation/example.py
@@ -8,4 +8,4 @@ if "error" in grade_data and grade_data['error'] == "course not found":
     print("Error: Course not found.")
 else:
     for course_grade_data in grade_data:
-            print("Number of students who received an A in {course} section {section} for semester {semester}: {num_a}".format(course = course_grade_data['course'], section = course_grade_data['section'], semester = course_grade_data['semester'], num_a = course_grade_data['A']))
+        print("Number of students who received an A in {course} section {section} for semester {semester}: {num_a}".format(course = course_grade_data['course'], section = course_grade_data['section'], semester = course_grade_data['semester'], num_a = course_grade_data['A']))

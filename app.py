bed3828d6211cab1fa086065111b33150c554c26
Liam DeVoe <orionldevoe@gmail.com>
2021-04-21 15:53:52 -0400

    linting fixes
    tabs -> spaces, trim trailing whitespace, add EOF newline, fix wrong indentation

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ import professors
 import grades
 
 urls = (
-	'/', 'Index',
-	'/v1', 'VersionOne',
-	'/v1/course', course.Course,
-	'/v1/courses', courses.Courses,
-	'/v1/professor', professor.Professor,
-	'/v1/professors', professors.Professors,
-	'/v1/grades', grades.Grades
+    '/', 'Index',
+    '/v1', 'VersionOne',
+    '/v1/course', course.Course,
+    '/v1/courses', courses.Courses,
+    '/v1/professor', professor.Professor,
+    '/v1/professors', professors.Professors,
+    '/v1/grades', grades.Grades
 )
 
 web.config.debug = False
@@ -25,20 +25,18 @@ app = web.application(urls, globals())
 render = web.template.render('templates', globals={'str': str})
 
 class Index:
-	def GET(self):
-		model.insert_view(web.ctx.host + web.ctx.fullpath, web.ctx.status, web.ctx.ip, web.ctx.env['HTTP_USER_AGENT'] if 'HTTP_USER_AGENT' in web.ctx.env else None, "GET")
-		return render.index()
+    def GET(self):
+        model.insert_view(web.ctx.host + web.ctx.fullpath, web.ctx.status, web.ctx.ip, web.ctx.env['HTTP_USER_AGENT'] if 'HTTP_USER_AGENT' in web.ctx.env else None, "GET")
+        return render.index()
 
 
 class VersionOne:
-	def GET(self):
-		model.insert_view(web.ctx.host + web.ctx.fullpath, web.ctx.status, web.ctx.ip, web.ctx.env['HTTP_USER_AGENT'] if 'HTTP_USER_AGENT' in web.ctx.env else None, "GET")
-		web.header('Content-Type', 'application/json')
-		web.header('Access-Control-Allow-Origin', '*')
-		return json.dumps({'version': 1, 'documentation': 'https://api.planetterp.com'})
+    def GET(self):
+        model.insert_view(web.ctx.host + web.ctx.fullpath, web.ctx.status, web.ctx.ip, web.ctx.env['HTTP_USER_AGENT'] if 'HTTP_USER_AGENT' in web.ctx.env else None, "GET")
+        web.header('Content-Type', 'application/json')
+        web.header('Access-Control-Allow-Origin', '*')
+        return json.dumps({'version': 1, 'documentation': 'https://api.planetterp.com'})
 
 
 if __name__ == "__main__":
-	app.run()
-
-
+    app.run()
--- utilities.py
+++ utilities.py
@@ -5,36 +5,36 @@ TRUE_FALSE = ['true', 'false']
 
 # modified from https://stackoverflow.com/q/354038
 def is_int(s):
-	try:
-		int(s)
-		return True
-	except ValueError:
-		return False
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
 
 def api_error(message):
-	web.ctx.status = "400 Bad Request"
-	return json.dumps({'error': message})
+    web.ctx.status = "400 Bad Request"
+    return json.dumps({'error': message})
 
 def get_limit(data):
-	limit = 100
+    limit = 100
 
-	if 'limit' in data:
-		limit = data['limit']
-		if not is_int(limit):
-			return api_error("limit parameter must be an integer between 1 and 1000")
-		limit = int(limit)
-		if limit < 1 or limit > 1000:
-			return api_error("limit parameter must be an integer between 1 and 1000")
-	return limit
+    if 'limit' in data:
+        limit = data['limit']
+        if not is_int(limit):
+            return api_error("limit parameter must be an integer between 1 and 1000")
+        limit = int(limit)
+        if limit < 1 or limit > 1000:
+            return api_error("limit parameter must be an integer between 1 and 1000")
+    return limit
 
 def get_offset(data):
-	offset = 0
+    offset = 0
 
-	if 'offset' in data:
-		offset = data['offset']
-		if not is_int(offset):
-			return api_error("offset parameter must be an integer greater than or equal to 0")
-		offset = int(offset)
-		if offset < 0:
-			return api_error("offset parameter must be an integer greater than or equal to 0")
-	return offset
+    if 'offset' in data:
+        offset = data['offset']
+        if not is_int(offset):
+            return api_error("offset parameter must be an integer greater than or equal to 0")
+        offset = int(offset)
+        if offset < 0:
+            return api_error("offset parameter must be an integer greater than or equal to 0")
+    return offset

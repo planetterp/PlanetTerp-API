b160f29c5a1e2ef43b7ad68908ab5f425d6ab99d
Liam DeVoe <orionldevoe@gmail.com>
2021-06-29 10:45:42 -0400

    fix courses.py using old method

--- courses.py
+++ courses.py
@@ -1,6 +1,7 @@
 import web
 import model
 import utilities
+from utilities import JsonBadRequest
 import json
 
 class Courses:
@@ -18,16 +19,16 @@ class Courses:
 
         if 'department' in data:
             if len(data['department']) != 4:
-                raise utilities.api_error("department parameter must be 4 characters")
+                raise JsonBadRequest("department parameter must be 4 characters")
 
             department = data['department']
 
             if not model.department_has_course(department):
-                raise utilities.api_error("no courses found with that department")
+                raise JsonBadRequest("no courses found with that department")
 
         if 'reviews' in data:
             if not data['reviews'] in utilities.TRUE_FALSE:
-                raise utilities.api_error("reviews parameter must be either true or false")
+                raise JsonBadRequest("reviews parameter must be either true or false")
 
             if data['reviews'] == 'true':
                 reviews = True
